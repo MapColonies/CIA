f50ef51b17529f3a627e4eb0e8c6149bdf040ee1
Vitali Ginzburg <54726763+vitaligi@users.noreply.github.com>
2020-12-14 18:00:55 +0200

    fix(cia): fix bad range extraction

--- src/utils/postgres_ranges.ts
+++ src/utils/postgres_ranges.ts
@@ -7,16 +7,19 @@ export function rangeFormatter(start: number, end: number, inclusion: Inclusion 
 export function getIntRangeBound(range: string, bound: Bound = 'lower', inclusion: Inclusion = 'closed'): number {
   const [lower, upper] = range.split(',');
   let add = 0;
+  let end;
   if (bound === 'lower') {
     const lowerBoundInclusion: Inclusion = lower.startsWith('[') ? 'closed' : 'open';
+    end = Number(lower.substring(1, lower.length));
     if (inclusion === 'closed') add += lowerBoundInclusion === 'closed' ? 0 : 1;
     else add -= lowerBoundInclusion === 'open' ? 0 : 1;
   } else {
     const upperBoundInclusion: Inclusion = upper.endsWith(']') ? 'closed' : 'open';
+    end = Number(upper.substring(0, upper.length - 1));
     if (inclusion === 'closed') add -= upperBoundInclusion === 'closed' ? 0 : 1;
     else add += upperBoundInclusion === 'open' ? 0 : 1;
   }
-  return Number(upper) + add;
+  return end + add;
 }
 
 export function rangeToObj(startKey: string, endKey: string, range: string): Record<string, number> {
